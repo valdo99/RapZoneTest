afcc5d61b068c8f66e9999d6250406e17a487560
valdo99 <edvaldo99@icloud.com>
2019-07-24 16:04:24 +0200

    fixing next button on no country selected

--- trapzone/src/App.js
+++ trapzone/src/App.js
@@ -5,6 +5,7 @@ import 'react-flags-select/scss/react-flags-select.scss';
 import ReactPlayer from 'react-player';
 import data from './data/data.js';
 import { Button } from 'semantic-ui-react';
+import header from './assets/header.png'
 
 let countries = ["AL", "AU", "AT", "BE", "BR", "CM", "CA", "CN", "DK", "EE", "FR", "GA", "DE", "GR", "IS", "ID", "IE", "IT", "JM", "JP", "NL", "NZ", "NG", "NO", "PT", "PR", "ZA", "KR", "ES", "SE", "CH", "TR", "GB", "SD", "SK", "HK", "GH", "PE", "SC", "RU", "KZ", "BY", "IN", "MA", "RO", "CO", "VE", "PL", "FI", "MZ", "TH", "AM", "ML", "PH", "UA", "IL", "CG", "AC"];
 class App extends React.Component {
@@ -54,34 +55,45 @@ class App extends React.Component {
     }
   }
   Next() {
-    let countryforNext = data.filter(c => c.country === this.state.selectedCountry)
-    let index = Math.round(0 + (Math.random() * (countryforNext.length)));
-    this.setState((state, props) => ({
-      YTurl: countryforNext[index].youtube
-    })
-    );
+    if (this.state.selectedCountry === '') {
+      let allIndex = Math.round(0 + (Math.random() * (data.length)));
+      this.setState((state, props) => ({
+        YTurl: data[allIndex].youtube
+      })
+      );
+
+    } else {
+      var countryforNext = data.filter(c => c.country === this.state.selectedCountry)
+      var index = Math.round(0 + (Math.random() * (countryforNext.length)));
+      this.setState((state, props) => ({
+        YTurl: countryforNext[index].youtube
+      })
+      );
+    }
   }
   render() {
 
     return (
       <center>
-        <div style={{ margin: '2%' }}>
-          <ReactFlagsSelect countries={countries}
-            searchable={true}
-            searchPlaceholder="Search for a country"
-            onSelect={this.onSelectFlag.bind(this)}
-          />
-        </div>
-        <div style={{ height: 600 }}>
-          <ReactPlayer
-            url={this.state.YTurl}
-            width='100%'
-            height='100%'
-            playing={this.state.pause}
-          />
-          <div style={{ margin: '2%' }}>
-            <Button content='Pause' icon='pause' labelPosition='left' onClick={this.Pause.bind(this)} />
-            <Button content='Next' icon='right arrow' labelPosition='right' onClick={this.Next.bind(this)} />
+        <div style={{ backgroundImage: header }}>
+          <div style={{ margin: '2%', backgroundImage: header }}>
+            <ReactFlagsSelect countries={countries}
+              searchable={true}
+              searchPlaceholder="Search for a country"
+              onSelect={this.onSelectFlag.bind(this)}
+            />
+          </div>
+          <div style={{ height: 600, backgroundImage: header }}>
+            <ReactPlayer
+              url={this.state.YTurl}
+              width='100%'
+              height='100%'
+              playing={this.state.pause}
+            />
+            <div style={{ margin: '2%' }}>
+              <Button content='Pause' icon='pause' labelPosition='left' onClick={this.Pause.bind(this)} />
+              <Button content='Next' icon='right arrow' labelPosition='right' onClick={this.Next.bind(this)} />
+            </div>
           </div>
         </div>
       </center>
